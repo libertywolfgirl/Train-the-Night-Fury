d9b2064e474af97f0c08038798b092a584beb8d7
Glitch (virtual-pet-laura) <none>
2019-07-09 17:19:49 +0000

    🕌💧 Checkpoint ./pet.js:5423097/284

--- pet.js
+++ pet.js
@@ -1,1 +1,7 @@
 console.log("pet script running")
+
+//select the button on our page that says 'click me'
+//print out the element that we've selected
+
+const petButton = document.queryselector ("button")
+console.log("pet button")
