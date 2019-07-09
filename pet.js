bd5da1d2a753ddd1ac8187a3518f801c95e1cb0f
Glitch (virtual-pet-laura) <none>
2019-07-09 17:30:51 +0000

    🐈🕶 Checkpoint ./pet.js:5423097/224

--- pet.js
+++ pet.js
@@ -3,5 +3,9 @@ console.log("pet script running")
 //select the button on our page that says 'click me'
 //print out the element that we've selected
 
-const petButton = document.queryselector ("button")
-console.log("pet button")
+const petButton = document.querySelector ("button")
+console.log(petButton)
+// Const refers to a variable 
+const divstatus = document.querySelector ("#status")
+
+const log = ("divstatus")
