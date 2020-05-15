b7f7a1ed4f02119fd8f6af18705ffafa494b253c
Glitch (virtual-pet-laura) <none>
2020-05-15 21:46:35 +0000

    💒🌝 Checkpoint ./pet.js:16231546/227

--- pet.js
+++ pet.js
@@ -2,17 +2,17 @@ console.log("pet script running");
 
 //select the button on our page that says 'click me'
 const petButton = document.querySelector("button");
+console.log(petButton);
+
 //initializa a variable called count
 let count = 0;
+
 //print out the element that we've selected
-console.log(petButton);
 petButton.addEventListener("click", e => {
   console.log("Button pressed!");
   count += 1;
+  console.log(`You pet the Night Fury ${count} times`);
 });
-console.log(`You pet the bunny $ {count} times`);
-//const
-console.log(petButton);
-// Const refers to a variable
+
 const divstatus = document.querySelector("#status");
 console.log(divstatus);
