cfa2f61137c5005fb8f952ed09a689db4d8b22d5
Glitch (virtual-pet-laura) <none>
2020-05-15 23:42:24 +0000

    🚖🚕 Checkpoint ./pet.js:16231546/3030

--- pet.js
+++ pet.js
@@ -2,33 +2,37 @@ console.log("pet script running");
 // variables
 let petCount;
 let gamePlaying;
-
-// select the button on our page that says 'click me'
 const petButton = document.querySelector("button");
-console.log(petButton);
 
-// create function to change image
-function changeImage() {
+// initialize game
+init();
+
+// play the game
+petButton.addEventListener("click", e => {
+  if (gamePlaying) {
+    // print out the element that we've selected
+    console.log("Button pressed!");
+    petCount += 1;
+    console.log(`You pet the Night Fury ${petCount} times`);
+
+    // check if the game has ended
+    endGame();
+  }
+});
+
+// create function to end the game
+function endGame() {
   let image = document.getElementById("nightFury");
   if (petCount > 15) {
     image.src = "img/grumpyBun.jpg";
-    document.querySelector("#status").textContent = 'You failed!';
+    document.querySelector("#status").textContent = "You failed!";
+    gamePlaying = false;
   } else {
     image.src = "img/happyBun.jpg";
   }
 }
 
-// print out the element that we've selected
-petButton.addEventListener("click", e => {
-  console.log("Button pressed!");
-  petCount += 1;
-  console.log(`You pet the Night Fury ${petCount} times`);
-
-  // check if too many pets
-  changeImage();
-});
-
-// initialize game
+// function to initialize game
 function init() {
   petCount = 0;
   gamePlaying = true;
