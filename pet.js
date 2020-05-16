5d6c1ba6502ce922c02d70e9b89d91740ef8e819
Glitch (virtual-pet-laura) <none>
2020-05-16 02:02:01 +0000

    ✂️⛺️ Checkpoint ./pet.js:16231546/1586

--- pet.js
+++ pet.js
@@ -1,12 +1,63 @@
 console.log("pet script running");
 // variables
 let petCount;
+let happiness;
+let weight;
 let gamePlaying;
 const image = document.getElementById("nightFury");
 const petButton = document.querySelector("button");
 
 // initialize game
 init();
+
+// When each button is clicked, it will "call" function for that button (functions are below)
+  $('.feed-button').click(clickedFeedButton);
+  $('.play-button').click(clickedPlayButton);
+  $('.pet-button').click(clickedPetButton);
+  $('.train-button').click(clickedTrainButton);
+
+  function clickedFeedButton() {
+    // Increase pet happiness
+    happiness += 1;
+    // Increase pet weight
+    weight += 1;
+    checkAndUpdatePetInfoInHtml();
+  }
+  
+  function clickedPlayButton() {
+    // Increase pet happiness
+    happiness += 1;
+    // Decrease pet weight
+    weight -= 1;
+    checkAndUpdatePetInfoInHtml();
+  }
+  
+  function clickedPetButton() {
+    // Decrease pet happiness
+    pet_info.happiness -= 1;
+    // Decrease pet weight
+    pet_info.weight -= 1;
+    checkAndUpdatePetInfoInHtml();
+  }
+
+  function clickedTrainButton() {
+    // Decrease pet happiness
+    pet_info.happiness -= 1;
+    // Decrease pet weight
+    pet_info.weight -= 1;
+    checkAndUpdatePetInfoInHtml();
+  }
+
+  function checkAndUpdatePetInfoInHtml() {
+    checkWeightAndHappinessBeforeUpdating();  
+    updatePetInfoInHtml();
+  }
+  
+  function checkWeightAndHappinessBeforeUpdating() {
+    // Add conditional so if weight is lower than zero, set it back to zero
+    if (pet_info.weight < 1) pet_info.weight = 1;
+    if (pet_info.happiness < 1) pet_info.happiness = 0;
+  }
 
 // play the game
 petButton.addEventListener("click", e => {
